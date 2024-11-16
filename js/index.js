774eef60f0b9d739afaf97e09f130d276e5e1b55
MeiKawanishi <xoxo.nyanchuuu@gmail.com>
2024-11-16 15:22:07 +0900

    update: 処理追加

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   feather.replace(); // Feather Iconsの初期化
+
+  /** コーディング規約 */
+  // dom取得はgetElementを使用しない
+  // VanillaJSのみ使用、省略記法可
+  // 要素は新しくJSで作成せず既存テンプレートを使用する
+  // 調べるのはOK、コピペNG
+
+  /** 処理準備 */
+  // テンプレートタグを使用し送信メッセージテンプレート、
+  // 自動応答メッセージテンプレートを作成してください。
+  // ・p-chatContents__messageTextと同様のもの
+  // ・p-chatContents__messageTextと兄弟要素にする
+  // ・templateタグを使用
+  // ・templateタグにid付与
+  // ・デフォルトで色は付与しない
+  // ・テキスト、時間は空欄にしておく、タグは記述しておく
+
+  /** ここから */
 });
